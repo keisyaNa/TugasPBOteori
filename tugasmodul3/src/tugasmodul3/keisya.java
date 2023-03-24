f14c11f9f5b9559332fbb35f34fa54405e7c657b
keisyaNa <keisya.doongie@gmail.com>
2023-03-24 23:40:36 +0700

    tugas modul 3

---
+++ tugasmodul3/src/tugasmodul3/keisya.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugasmodul3;
+
+/**
+ *
+ * @author ACER
+ */
+public class keisya {
+    public static void main(String[] args ){
+       System.out.println("Welcome to Java Programming Keisya<3");
+    }
+}
